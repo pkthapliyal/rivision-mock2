a55b752be0d9f250a8b771d5c11070dc56d65168
Pankaj Kumar <121335947+pkthapliyal@users.noreply.github.com>
2023-08-14 19:23:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const port = 3030;
 const cookiesParser = require("cookie-parser")
 const app = express();
+const cors= require("cors")
 const { connection } = require("./config/db")
 
 //  swagger set-up ;
@@ -39,6 +40,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specification));
 
 app.use(express.json());
 app.use(cookiesParser())
+app.use(cors())
 
 //  routers 
 const { userRoute } = require("./routes/user.route");
